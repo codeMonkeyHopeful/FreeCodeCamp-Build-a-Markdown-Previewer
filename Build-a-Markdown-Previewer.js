785d0247ae87eb8f983747a106b623ee078561a4
Ryan Jasinski <rjasinski@indeed.com>
2021-06-19 12:32:28 -0400

    framework in place

--- Build-a-Markdown-Previewer.js
+++ Build-a-Markdown-Previewer.js
@@ -1,10 +1,21 @@
 // import React from "react";
 const Application = () => {
   return (
-    <div className="main" className="row">
-      working
+    <div class="text-center">
+      <div className="main">App Component</div>
+      <Child />
     </div>
   );
 };
+
+class Child extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>I'm the child!</div>;
+  }
+}
 
 ReactDOM.render(<Application />, document.getElementById("root"));
